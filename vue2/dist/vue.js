d18f75c8b9753937d37cce2d93be9755e1efd750
‘mayulong’ <mayulong@suyechina.com>
2023-02-27 21:00:07 +0800

    代码生成0%

--- vue2/dist/vue.js
+++ vue2/dist/vue.js
@@ -16,11 +16,11 @@
   // 对模板进行编译处理
   function parseHTML(html) {
     // html 最开始肯定是一个 <div></div>
-
     const ELEMENT_TYPE = 1;
     const TEXT_TYPE = 3;
     const stack = []; // 用于存放元素的
     let currentParent; // 指向的是栈中的最后一个
+    let root;
     // 最终需要转化成一颗抽象语法树
 
     function createASTElement(tag, attrs) {
@@ -36,6 +36,7 @@
     // 利用栈型结构 来构造一棵树
     function start(tag, attrs) {
       let node = createASTElement(tag, attrs); // 创造一个 ast 节点
+      if (!root) root = node; // 如果为空就当成树的根节点
 
       if (currentParent) {
         node.parent = currentParent; // 只赋予了 parent 属性
@@ -115,13 +116,71 @@
         }
       }
     }
+
+    return root;
+  }
+
+  function genProps(attrs) {
+    let str = ''; // {name,value}
+    for (let i = 0; i < attrs.length; i++) {
+      let attr = attrs[i];
+      if (attr.name === 'style') {
+        let obj = {};
+        attr.value.split(';').forEach(item => {
+          let [key, value] = item.split(':');
+          obj[key] = value;
+        });
+        attr.obj = obj;
+      }
+      str += `${attr.name}:${JSON.stringify(attr.value)},`;
+    }
+    return `{${str.slice(0, -1)}}`;
+  }
+  const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g;
+  function gen(node) {
+    if (node.type === 1) {
+      return codegen(node);
+    } else {
+      // 文本
+      let text = node.text;
+      if (!defaultTagRE.test(text)) {
+        return `_v(${JSON.stringify(text)})`;
+      } else {
+        let tokens = [];
+        let match;
+        defaultTagRE.lastIndex = 0;
+        let lastIndex = 0;
+        while (match = defaultTagRE.exec(text)) {
+          let index = match.index; // 匹配的位置
+          if (index > lastIndex) {
+            tokens.push(JSON.stringify(text.slice(lastIndex, index)));
+          }
+          tokens.push(`_s(${match[1].trim()})`);
+          lastIndex = index + match[0].length;
+        }
+        if (lastIndex < text.length) {
+          tokens.push(JSON.stringify(text.slice(lastIndex)));
+        }
+        return `_v(${tokens.join('+')})`;
+      }
+    }
+  }
+  function genChildren(children) {
+    return children.map(child => gen(child));
+  }
+  function codegen(ast) {
+    let children = genChildren(ast.children);
+    let code = `_c('${ast.tag}',${ast.attrs.length > 0 ? genProps(ast.attrs) : 'null'}${ast.children.length ? `, ${children}` : ''})`;
+    return code;
   }
 
   // 对模板进行编译
   function compileToFunction(template) {
     // 1. 就是将 template 转化成 ast 语法树
-    parseHTML(template);
+    let ast = parseHTML(template);
     // 2. 生成 render 方法（render方法执行后的返回值的结果就是虚拟 dom）
+
+    console.log(codegen(ast));
   }
 
   // 我们希望重写数组中的部分方法
--- vue2/src/compiler/index.js
+++ vue2/src/compiler/index.js
@@ -1,118 +1,65 @@
-const ncname = `[a-zA-Z_][\\-\\.0-9_a-zA-Z]*`;
-const qnameCapture = `((?:${ncname}\\:)?${ncname})`;
-const startTagOpen = new RegExp(`^<${qnameCapture}`); // 他匹配到的分组是一个 标签名  <xxx 匹配到的是开始 标签的名字
-const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`);  // 匹配的是</xxxx>  最终匹配到的分组就是结束标签的名字
-const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/;  // 匹配属性
-// 第一个分组就是属性的key value 就是 分组3/分组4/分组五
-const startTagClose = /^\s*(\/?)>/;  // <div> <br/>
+import { parseHTML } from "./parse";
+
+function genProps(attrs) {
+  let str = ''; // {name,value}
+  for (let i = 0; i < attrs.length; i++) {
+    let attr = attrs[i];
+    if (attr.name === 'style') {
+      let obj = {};
+      attr.value.split(';').forEach(item => {
+        let [key, value] = item.split(':')
+        obj[key] = value;
+      })
+      attr.obj = obj;
+    }
+    str += `${attr.name}:${JSON.stringify(attr.value)},`
+  }
+  return `{${str.slice(0, -1)}}`
+}
+
 const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g
 
+function gen(node) {
+  if (node.type === 1) {
+    return codegen(node);
+  } else {
+    // 文本
+    let text = node.text;
+    if (!defaultTagRE.test(text)) {
+      return `_v(${JSON.stringify(text)})`
+    } else {
+      let tokens = [];
+      let match;
+      defaultTagRE.lastIndex = 0;
+      let lastIndex = 0;
+      while (match = defaultTagRE.exec(text)) {
+        let index = match.index; // 匹配的位置
+        if (index > lastIndex) {
+          tokens.push(JSON.stringify(text.slice(lastIndex, index)));
+        }
+        tokens.push(`_s(${match[1].trim()})`)
 
-// vue3 采用的不是使用正则
-// 对模板进行编译处理
-function parseHTML(html) { // html 最开始肯定是一个 <div></div>
-
-  const ELEMENT_TYPE = 1;
-  const TEXT_TYPE = 3;
-  const stack = []; // 用于存放元素的
-  let currentParent; // 指向的是栈中的最后一个
-  let root;
-  // 最终需要转化成一颗抽象语法树
-
-  function createASTElement(tag, attrs) {
-    return {
-      tag: tag,
-      type: ELEMENT_TYPE,
-      children: [],
-      attrs,
-      parent: null,
-    }
-  }
-
-  // 利用栈型结构 来构造一棵树
-  function start(tag, attrs) {
-    let node = createASTElement(tag, attrs); // 创造一个 ast 节点
-    if (!root) root = node; // 如果为空就当成树的根节点
-
-    if (currentParent) {
-      node.parent = currentParent; // 只赋予了 parent 属性
-      currentParent.children.push(node); // 还要让父亲记住自己
-    }
-
-    stack.push(node);
-    currentParent = node; // currentParent 为栈中的最后一个
-
-  }
-  function chars(text) { // 文本直接放到当前指向的节点
-    text = text.replace(/\s/g, ''); // 如果空格超过 2 个就删除2个以上的
-    text && currentParent.children.push({
-      type: TEXT_TYPE,
-      text,
-      parent: currentParent,
-    })
-  }
-  function end(tag) {
-    let node = stack.pop(); // 弹出最后一个，校验标签是否合法
-    currentParent = stack[stack.length - 1];
-  }
-  function advance(n) {
-    html = html.substring(n);
-  }
-  function parseStartTag() {
-    const start = html.match(startTagOpen);
-    if (start) {
-      const match = {
-        tagName: start[1], // 标签名
-        attrs: []
+        lastIndex = index + match[0].length;
       }
-      advance(start[0].length)
-      // 如果不是开始标签的结束 就一直匹配下去
-      let attr, end;
-      while (!(end = html.match(startTagClose)) && (attr = html.match(attribute))) {
-        advance(attr[0].length);
-        match.attrs.push({
-          name: attr[1],
-          value: attr[3] || attr[4] || attr[5] || true
-        })
+      if (lastIndex < text.length) {
+        tokens.push(JSON.stringify(text.slice(lastIndex)))
       }
-      if (end) {
-        advance(end[0].length);
-      }
-      return match;
-    }
-
-    return false; // 不是开始标签
-  }
-  while (html) {
-    // 如果 textEnd 为0 说明是一个开始标签或者结束标签
-    // 如果textEnd > 0 说明就是个文本的结束位置
-    let textEnd = html.indexOf('<'); // 如果 indexOf 中的索引是 0 则说明是个标签
-
-    if (textEnd == 0) {
-      const startTagMatch = parseStartTag();
-
-      if (startTagMatch) {
-        start(startTagMatch.tagName, startTagMatch.attrs)
-        continue;
-      }
-
-      let endTagMatch = html.match(endTag);
-      if (endTagMatch) {
-        advance(endTagMatch[0].length);
-        end(endTagMatch[1])
-        continue;
-      }
-    }
-    if (textEnd > 0) {
-      let text = html.substring(0, textEnd); // 文本内容
-
-      if (text) {
-        chars(text);
-        advance(text.length); // 解析到的文本
-      }
+      return `_v(${tokens.join('+')})`
     }
   }
 }
+
+function genChildren(children) {
+  return children.map(child => gen(child))
+}
+
+function codegen(ast) {
+  let children = genChildren(ast.children);
+  let code = `_c('${ast.tag}',${ast.attrs.length > 0 ? genProps(ast.attrs) : 'null'}${ast.children.length ? `, ${children}` : ''
+    })`
+  return code;
+}
+
 
 // 对模板进行编译
 export function compileToFunction(template) {
@@ -120,5 +67,8 @@ export function compileToFunction(template) {
   // 1. 就是将 template 转化成 ast 语法树
   let ast = parseHTML(template);
   // 2. 生成 render 方法（render方法执行后的返回值的结果就是虚拟 dom）
+
+
+  console.log(codegen(ast));
 
 }
